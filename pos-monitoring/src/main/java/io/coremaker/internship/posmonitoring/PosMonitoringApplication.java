7672ddc00a3646a9fac99ad430c68e949d8e6401
TOMA Victor-Stefan <viky.toma15@gmail.com>
2023-08-15 14:46:43 +0300

    Created the SpringBoot skeleton

---
+++ pos-monitoring/src/main/java/io/coremaker/internship/posmonitoring/PosMonitoringApplication.java
@@ -0,0 +1,13 @@
+package io.coremaker.internship.posmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosMonitoringApplication.class, args);
+	}
+
+}
