5055b6327067ac9fece680c586f0d5e0d91e2d86
George Berar <georgeberar.dev@gmail.com>
2023-08-24 12:14:54 +0300

    refactored

--- pos-monitoring/src/main/java/io/coremaker/internship/posmonitoring/controllers/PosController.java
+++ pos-monitoring/src/main/java/io/coremaker/internship/posmonitoring/controllers/PosController.java
@@ -2,23 +2,23 @@ package io.coremaker.internship.posmonitoring.controllers;
 
 import io.coremaker.internship.posmonitoring.controllers.dto.CreatePosDeviceRequestDto;
 import io.coremaker.internship.posmonitoring.controllers.dto.PosDeviceResponseDto;
-import io.coremaker.internship.posmonitoring.domain.PosDevice;
 import io.coremaker.internship.posmonitoring.services.PosService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/devices")
 public class PosController {
 
     private final PosService posService;
 
-    @PostMapping("/pos")
+    @PostMapping
     PosDeviceResponseDto createPosDevice(@RequestBody CreatePosDeviceRequestDto body) {
         return posService.createPosDevice(body);
     }
-
 
 }
