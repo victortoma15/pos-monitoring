cc596c987a2aa40708be634a4139c69009fd0837
Toma Victor-Stefan <94327403+victortoma15@users.noreply.github.com>
2023-09-22 11:47:13 +0300

    Done the update endpoint (#13)
    * Done the update endpoint
    
    * Done the update endpoint

--- pos-monitoring/src/main/java/io/coremaker/internship/posmonitoring/application/grpc/posdevice/PosDeviceGrpcController.java
+++ pos-monitoring/src/main/java/io/coremaker/internship/posmonitoring/application/grpc/posdevice/PosDeviceGrpcController.java
@@ -3,6 +3,7 @@ package io.coremaker.internship.posmonitoring.application.grpc.posdevice;
 import io.coremaker.internship.posmonitoring.*;
 import io.coremaker.internship.posmonitoring.domain.model.PosDevice;
 import io.coremaker.internship.posmonitoring.domain.model.command.CreatePosDeviceCommand;
+import io.coremaker.internship.posmonitoring.domain.model.command.UpdatePosDeviceCommand;
 import io.coremaker.internship.posmonitoring.domain.port.PosDeviceServicePort;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
@@ -19,6 +20,7 @@ public class PosDeviceGrpcController extends PosDeviceServiceGrpc.PosDeviceServi
 
     @Override
     public void addPosDevice(CreatePosDeviceRequest.CreatePosDeviceRequestGrpc request, StreamObserver<CreatePosDeviceResponse.CreatePosDeviceResponseGrpc> responseObserver) {
+
         CreatePosDeviceCommand command = new CreatePosDeviceCommand();
         command.setDeviceId(request.getDeviceId());
         command.setLocation(request.getLocation());
@@ -63,5 +65,28 @@ public class PosDeviceGrpcController extends PosDeviceServiceGrpc.PosDeviceServi
         responseObserver.onCompleted();
     }
 
+    @Override
+    public void updatePosDevice(UpdatePosDeviceStatusRequest.UpdatePosDeviceRequestGrpc request, StreamObserver<UpdatePosDeviceStatusResponse.UpdatePosDeviceResponseGrpc> responseObserver) {
 
+        UpdatePosDeviceCommand command = new UpdatePosDeviceCommand();
+        command.setId(request.getId());
+        command.setOnline(request.getOnline());
+        command.setLastActivity(Instant.parse(request.getLastActivity()));
+
+        PosDevice updatedPosDevice = posDeviceServicePort.update(command);
+
+        UpdatePosDeviceStatusResponse.UpdatePosDeviceResponseGrpc response = UpdatePosDeviceStatusResponse.UpdatePosDeviceResponseGrpc.newBuilder()
+                .setId(updatedPosDevice.getId())
+                .setDeviceId(updatedPosDevice.getDeviceId())
+                .setLocation(updatedPosDevice.getLocation())
+                .setProvider(updatedPosDevice.getProvider())
+                .setOnline(updatedPosDevice.getOnline())
+                .setLastActivity(updatedPosDevice.getLastActivity().toString())
+                .setCreatedAt(updatedPosDevice.getCreatedAt().toString())
+                .setUpdatedAt(updatedPosDevice.getUpdatedAt().toString())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
